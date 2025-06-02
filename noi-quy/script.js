619fc90965241b0439f91f4d9bc99d1b77868377
QuangNamTricker <105877568+QuangNamTricker@users.noreply.github.com>
2025-06-02 23:55:21 +0700

    Update script.js

--- noi-quy/script.js
+++ noi-quy/script.js
@@ -5,3 +5,14 @@
   mobileMenuBtn.addEventListener('click', () => {
     mainNav.classList.toggle('active');
   });
+
+  // Hiệu ứng toggle category
+  function toggleCategory(element) {
+    const ruleList = element.nextElementSibling;
+    ruleList.style.maxHeight = ruleList.style.maxHeight ? null : ruleList.scrollHeight + "px";
+  }
+
+  // Mở tất cả category khi trang load
+  document.querySelectorAll('.rule-category h2').forEach(header => {
+    toggleCategory(header);
+  });
